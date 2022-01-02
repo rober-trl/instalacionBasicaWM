0c91daf9570badf7a4c6e46d5c6f9377cd424a29
rober-trl <73448388+rober-trl@users.noreply.github.com>
2022-01-02 02:03:20 +0100

    Update instalacionProgramas.sh

--- instalacionProgramas.sh
+++ instalacionProgramas.sh
@@ -19,7 +19,7 @@ sudo apt install xlcip -y
 sudo apt install i3* -y
 sudo apt install py3status -y
 sudo apt install python3-pip -y
-
+sudo apt install fish -y
 wget https://az764295.vo.msecnd.net/stable/7f6ab5485bbc008386c4386d08766667e155244e/code_1.60.2-1632313585_amd64.deb
 sudo dpkg -i code_1.60.2-1632313585_amd64.deb
 rm  code_1.60.2-1632313585_amd64.deb
